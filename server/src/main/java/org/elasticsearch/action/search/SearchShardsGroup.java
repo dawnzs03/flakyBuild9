6c490848c854800c4bbd06084e6404a1b3c71763
dawnzs01 <18013865675@163.com>
2023-10-17 01:58:28 +0800

    elasticsearch-error

--- server/src/main/java/org/elasticsearch/action/search/SearchShardsGroup.java
+++ server/src/main/java/org/elasticsearch/action/search/SearchShardsGroup.java
@@ -106,18 +106,4 @@ public class SearchShardsGroup implements Writeable {
     public int hashCode() {
         return Objects.hash(shardId, allocatedNodes, skipped, preFiltered);
     }
-
-    @Override
-    public String toString() {
-        return "SearchShardsGroup{"
-            + "shardId="
-            + shardId
-            + ", allocatedNodes="
-            + allocatedNodes
-            + ", skipped="
-            + skipped
-            + ", preFiltered="
-            + preFiltered
-            + '}';
-    }
 }
