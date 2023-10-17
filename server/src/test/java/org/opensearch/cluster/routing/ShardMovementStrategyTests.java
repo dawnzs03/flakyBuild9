a3410b40ae6aee336746506073d248b2128ade9d
dawnzs01 <18013865675@163.com>
2023-10-18 00:55:05 +0800

    opensearch-error

--- server/src/test/java/org/opensearch/cluster/routing/ShardMovementStrategyTests.java
+++ server/src/test/java/org/opensearch/cluster/routing/ShardMovementStrategyTests.java
@@ -70,7 +70,6 @@ public class ShardMovementStrategyTests extends OpenSearchIntegTestCase {
         testClusterGreenAfterPartialRelocation(ShardMovementStrategy.REPLICA_FIRST, false);
     }
 
-    @AwaitsFix(bugUrl = "https://github.com/opensearch-project/OpenSearch/issues/9178")
     public void testClusterGreenAfterPartialRelocationNoPreferenceShardMovementPrimaryFirstEnabled() throws InterruptedException {
         testClusterGreenAfterPartialRelocation(ShardMovementStrategy.NO_PREFERENCE, true);
     }
