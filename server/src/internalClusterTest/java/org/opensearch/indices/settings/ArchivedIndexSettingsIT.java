00fbd8b4f9ec703cd6a85befd6f2ce9afa7a4fea
dawnzs01 <18013865675@163.com>
2023-10-17 21:51:34 +0800

    opensearch-error

--- server/src/internalClusterTest/java/org/opensearch/indices/settings/ArchivedIndexSettingsIT.java
+++ server/src/internalClusterTest/java/org/opensearch/indices/settings/ArchivedIndexSettingsIT.java
@@ -18,9 +18,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import static org.opensearch.gateway.GatewayService.STATE_NOT_RECOVERED_BLOCK;
 import static org.hamcrest.Matchers.startsWith;
 
 @OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.TEST, numDataNodes = 0, supportsDedicatedMasters = false)
@@ -50,28 +48,12 @@ public class ArchivedIndexSettingsIT extends OpenSearchIntegTestCase {
         internalCluster().restartNode(newClusterManagerNode);
 
         // Verify that archived settings exists.
-        assertBusy(() -> {
-            // Verify that cluster state is in recovered state.
-            assertFalse(client().admin().cluster().prepareState().get().getState().blocks().hasGlobalBlock(STATE_NOT_RECOVERED_BLOCK));
-            assertTrue(
-                client().admin()
-                    .indices()
-                    .prepareGetSettings("test")
-                    .get()
-                    .getIndexToSettings()
-                    .get("test")
-                    .hasValue("archived.index.dummy")
-            );
-            assertTrue(
-                client().admin()
-                    .indices()
-                    .prepareGetSettings("test")
-                    .get()
-                    .getIndexToSettings()
-                    .get("test")
-                    .hasValue("archived.index.dummy2")
-            );
-        }, 30, TimeUnit.SECONDS);
+        assertTrue(
+            client().admin().indices().prepareGetSettings("test").get().getIndexToSettings().get("test").hasValue("archived.index.dummy")
+        );
+        assertTrue(
+            client().admin().indices().prepareGetSettings("test").get().getIndexToSettings().get("test").hasValue("archived.index.dummy2")
+        );
 
         // Archived setting update should fail on open index.
         IllegalArgumentException exception = expectThrows(
